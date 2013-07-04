d608896fbecb936adef159e6aafb83ada17a675e
Malik Olivier Boussejra <malik.boussejra@eleves.ec-nantes.fr>
2013-07-04 23:59:55 +0200

    ASCMII, only Login page is here for the time being

---
+++ ASCMII/project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "ASCMII"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    "mysql" % "mysql-connector-java" % "5.1.18",
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
