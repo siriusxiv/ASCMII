73cefc10286f4a4307cd5e555efe1d295aa1970e
malik <malik.boussejra@eleves.ec-nantes.fr>
2013-08-23 14:30:37 +0200

    Added forgotten license

--- app/functions/agap/Matiere.java
+++ app/functions/agap/Matiere.java
@@ -1,3 +1,24 @@
+/****************************************************************************
+
+	ASCMII is a web application developped for the Ecole Centrale de Nantes
+	aiming to organize quizzes during courses or lectures.
+    Copyright (C) 2013  Malik Olivier Boussejra
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+
+******************************************************************************/
+
 package functions.agap;
 
 import functions.AGAPUtil;
